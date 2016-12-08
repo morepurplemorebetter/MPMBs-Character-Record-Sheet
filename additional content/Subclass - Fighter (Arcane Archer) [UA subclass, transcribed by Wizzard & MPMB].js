f46040941ed5f1c1c0ce2fdc7de9d3fd641f7c5b
morepurplemorebetter <flapkan@gmail.com>
2016-12-08 17:59:32 +0100

    first commit

---
+++ additional content/Subclass - Fighter (Arcane Archer) [UA subclass, transcribed by Wizzard & MPMB].js
@@ -0,0 +1,109 @@
+/*  -WHAT IS THIS?-
+    The script featured here is made as an optional addition to "MPMB's Character Record Sheet" found at http://bit.ly/MPMBCharTools
+    You can add the content to the Character Sheet's functionality by adding the script below in the "Add Custom Script" dialogue.
+     
+    -KEEP IN MIND-
+    Note that you can add as many custom codes as you want, but you have to add the code in at once (i.e. copy all the code into a single, long file and copy that into the sheet).
+    It is recommended to enter the code in a fresh sheet before adding any other information.
+*/
+ 
+/*  -INFORMATION-
+    Subject:    Subclass
+    Effect:     This script adds a subclass for the Fighter, called "Arcane Archer"
+                This is taken from the Unearthed Arcana: Fighter Martial Archetypes (http://dnd.wizards.com/articles/unearthed-arcana/fighter)
+    Code by:    Wizzard & MorePurpleMoreBetter
+    Date:       2016-12-08 (sheet v12.64)
+*/
+ 
+ClassSubList["arcane archer"] = {
+    regExpSearch : /^(?=.*arcane)(?=.*archer).*$/i,
+    subname : "Arcane Archer",
+    fullname : "Arcane Archer",
+    features : {
+        "subclassfeature3" : {
+            name : "Arcane Arrow",
+            source : ["UA:FMA", 1],
+            minlevel : 3,
+            description : "\n   " + "As a bonus action, I can create one magical arrow that I can fire with a bow" + "\n   " + "A shot with the arrow counts as magical and does additional force damage on a hit" + "\n   " + "When I create the arrow, I can apply one of my known Arcane Shots on it" + "\n   " + "This arrow lasts until the end of my turn or until I hit or miss a target with it",
+            additional : ["", "", "+2d6 force damage", "+2d6 force damage", "+2d6 force damage", "+2d6 force damage", "+2d6 force damage", "+2d6 force damage", "+2d6 force damage", "+2d6 force damage", "+2d6 force damage", "+2d6 force damage", "+2d6 force damage", "+2d6 force damage", "+2d6 force damage", "+2d6 force damage", "+2d6 force damage", "+4d6 force damage", "+4d6 force damage", "+4d6 force damage"],
+            usages : 2,
+            recovery : "short rest",
+            eval : "AddAction(\"bonus action\", \"Create Magical Arrow\", \"Arcane Archer (Arcane Arrow)\");",
+			removeeval : "RemoveAction(\"bonus action\", \"Create Magical Arrow\")",
+        },
+        "subclassfeature3.1" : {
+            name : "Arcane Shot",
+            source : ["UA:FMA", 1],
+            minlevel : 3,
+            description : "\n   " + "Use the \"Choose Features\" button above to add Arcane Shots to the third page",
+            additional : ["", "", "2 arcane shots known", "2 arcane shots known", "2 arcane shots known", "2 arcane shots known", "3 arcane shots known", "3 arcane shots known", "3 arcane shots known", "4 arcane shots known", "4 arcane shots known", "4 arcane shots known", "4 arcane shots known", "4 arcane shots known", "5 arcane shots known", "5 arcane shots known", "5 arcane shots known", "6 arcane shots known", "6 arcane shots known", "6 arcane shots known"],
+            extraname : "Arcane Shot",
+            extrachoices : ["Beguiling Arrow", "Brute Bane Arrow", "Bursting Arrow", "Defending Arrow", "Grasping Arrow", "Piercing Arrow", "Seeking Arrow", "Shadow Arrow"],
+            "beguiling arrow" : {
+                name : "Beguiling Arrow",
+                source : ["UA:FMA", 1],
+                description : " [Enchantment]" + "\n   " + "If the arrow hits, I choose an ally withing 30 feet of the target" + "\n   " + "The target can't attack the chosen ally or include the ally in any harmful area of effects" + "\n   " + "This effect stops if the target is immune to charm effects or the ally damages the target",
+            },
+            "brute bane arrow" : {
+                name : "Brute Bane Arrow",
+                source : ["UA:FMA", 1],
+                description : " [Necromancy]" + "\n   " + "If the arrow hits, the target's attacks deal half damage until the end of my next turn" + "\n   " + "Only attacks that deal bludgeoning, piercing or slashing damage are halved",
+            },
+            "bursting arrow" : {
+                name : "Bursting Arrow",
+                source : ["UA:FMA", 2],
+                description : " [Evocation]" + "\n   " + "If the arrow hits, all creatures within 10 ft of the target creature take 2d6 force damage",
+            },
+            "defending arrow" : {
+                name : "Defending Arrow",
+                source : ["UA:FMA", 2],
+                description : " [Abjuration]" + "\n   " + "If the arrow hits, the target has disadv. on its next attack before the end of my next turn",
+            },
+            "grasping arrow" : {
+                name : "Grasping Arrow",
+                source : ["UA:FMA", 2],
+                description : "[Conjuration Magic]" + "\n   " + "If the arrow hits, the target is wrapped with grasping, thorny brambles for 1 minute" + "\n   " + "The target has -10 ft speed; It takes 2d6 slashing damage when moving more than 1 ft" + "\n   " + "As an action, the target or a creature can remove the brambles with a DC 10 Str check",
+            },
+            "piercing arrow" : {
+                name : "Piercing Arrow",
+                source : ["UA:FMA", 2],
+                description : " [Transmutation]" + "\n   " + "The arrow transform into an ethereal dart that creates a line of 1 ft wide and 30 ft long" + "\n   " + "I then make a separate attack using my Arcane Arrow against each creature in that line",
+            },
+            "seeking arrow" : {
+                name : "Seeking Arrow",
+                source : ["UA:FMA", 2],
+                description : " [Divination]" + "\n   " + "As an action, I can make a ranged attack against a creature I have seen in the last minute" + "\n   " + "The seeking arrow moves around corners and obstacles to hit the target" + "\n   " + "The attack ignores 1/2 and 3/4 cover and disadvantage from range or being out of sight" + "\n   " + "The attack misses if the target is too far away or there is no path for the arrow to travel" + "\n   " + "I know if the arrow hits the target, but don't learn the location unless it's in line of sight",
+				action : ["action", ""]
+            },
+            "shadow arrow" : {
+                name : "Shadow Arrow",
+                source : ["UA:FMA", 2],
+                description : " [Illusion]" + "\n   " + "If the arrow hits, the target can't see beyond 30 ft until the end of my next turn",
+            },
+        },
+        "subclassfeature3.2" : {
+            name : "Archer's Lore",
+            source : ["UA:FMA", 1],
+            minlevel : 3,
+            description : "\n   " + "I gain proficiency with two skills" + "\n   " + "I can choose from: Arcana, Athletics, Nature, Perception, Stealth, or Survival",
+            skillstxt : "\n\n" + toUni("Arcane Archer") + ": Choose two from Arcana, Athletics, Nature, Perception, Stealth, and Survival.",
+        },
+        "subclassfeature7" : {
+            name : "Conjure Arrows",
+            source : ["UA:FMA", 1],
+            minlevel : 7,
+            description : "\n   " + "As an action, I can create up to 20 nonmagical arrows that remain for 10 minutes" + "\n   " + "The arrows vanish if I use this feature again with 10 minutes",
+			action : ["action", ""],
+        },
+        "subclassfeature15" : {
+            name : "Ever-Ready Arrow",
+            source : ["UA:FMA", 1],
+            minlevel : 15,
+            description : "\n   " + "I regain one use of Arcane Arrow one minute after I expend my last remaining use of it"
+        },
+    }
+}
+ 
+ClassList.fighter.subclasses[1].push("arcane archer");
+ 
+SourceList["UA:FMA"] = {name : "Unearthed Arcana: Fighter Martial Archetypes", abbreviation : "UA:FMA"};
