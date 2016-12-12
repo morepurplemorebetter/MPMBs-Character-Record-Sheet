5cf510f4e10882b3970439fbbb8697bb6c5024a7
morepurplemorebetter <flapkan@gmail.com>
2016-12-12 01:00:08 +0100

    v12.65

--- _variables/Lists.js
+++ _variables/Lists.js
@@ -293,7 +293,7 @@ var CurrentCompRace = {};
 
 var CurrentSpells = {};
 var CurrentCasters = {};
-var CurrentResources = {};
+var CurrentSources = {};
 var UpdateSpellSheets = {};
 
 var ExperiencePointsList = ["", 300, 900, 2700, 6500, 14000, 23000, 34000, 48000, 64000, 85000, 100000, 120000, 140000, 165000, 195000, 225000, 265000, 305000, 355000, 1000000000];
