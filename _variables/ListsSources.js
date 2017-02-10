1c2572eee9fc4e75ec671233ad54fa62f47f8682
morepurplemorebetter <flapkan@gmail.com>
2017-02-10 07:44:57 +0100

    Remove duplicate UA:A

--- _variables/ListsSources.js
+++ _variables/ListsSources.js
@@ -165,12 +165,6 @@ var SourceList = {
 		name : "Unearthed Arcana: Artificer", //2017-01-09
 		abbreviation : "UA:A",	
 		group : "Unearthed Arcana",
-		url : "http://media.wizards.com/2016/dnd/downloads/UAPaladin_SO_20161219_1.pdf"
-	},
-	"UA:A" : {
-		name : "Unearthed Arcana: Artificer", //2017-01-09
-		abbreviation : "UA:A",	
-		group : "Unearthed Arcana",
 		url : "http://media.wizards.com/2016/dnd/downloads/1_UA_Artificer_20170109.pdf"
 	},
 	"UA:RnR" : {
