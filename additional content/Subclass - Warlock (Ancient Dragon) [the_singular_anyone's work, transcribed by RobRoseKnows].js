9672c2a733d12c6aaaec1d93ca52badf02d75e6e
Rob Rose <bertra97@gmail.com>
2017-05-30 23:10:41 -0400

    Worked on dragon patron more.

--- additional content/Subclass - Warlock (Ancient Dragon) [the_singular_anyone's work, transcribed by RobRoseKnows].js
+++ additional content/Subclass - Warlock (Ancient Dragon) [the_singular_anyone's work, transcribed by RobRoseKnows].js
@@ -37,101 +37,101 @@ ClassSubList["ancient dragon"] = {
 			action : ["action", ""],
 		},
 		"subclassfeature1.1": {
-				name : "Dragon Patron",
-				source : ["HB", 0],
-				minlevel : 1,
-				description : "\n   " + "Choose an Ancient Dragon Patron using the \"Choose Feature\" button above.",
-				choices : ["Black Dragon Patron", "Blue Dragon Patron", "Brass Dragon Patron", "Bronze Dragon Patron", "Copper Dragon Patron", "Gold Dragon Patron", "Green Dragon Patron", "Red Dragon Patron", "Silver Dragon Patron", "White Dragon Patron"],
-				"black dragon patron" : {
-					name : "Black Dragon Patron",
-					description : "\n   " + "I have draconic ancestry with black dragons, which are affiliated with acid damage" + "\n   " + "When interacting with dragons, if I can add my proficiency bonus, I can double it",
-					eval : 	"var ToAdd = [\"warlock\", \"subclassfeature6\", \"black\"]; " + 
-							"if (classes.known.warlock.level >= 6 && tDoc.getField(\"Class Features Remember\").value.indexOf(ToAdd.toString()) === -1) { " + 
-								"ClassFeatureOptions(ToAdd); " + 
-							"} ",
-					dragonElement : "acid"
-				},
-				"blue dragon patron" : {
-					name : "Blue Dragon Patron",
-					description : "\n   " + "I have draconic ancestry with blue dragons, which are affiliated with lightning damage" + "\n   " + "When interacting with dragons, if I can add my proficiency bonus, I can double it",
-					eval : 	"var ToAdd = [\"warlock\", \"subclassfeature6\", \"blue\"]; " + 
-							"if (classes.known.warlock.level >= 6 && tDoc.getField(\"Class Features Remember\").value.indexOf(ToAdd.slice(0,3).toString()) === -1) { " + 
-								"ClassFeatureOptions(ToAdd); " +
-							"} ",
-					dragonElement : "lightning"
-				},
-				"brass dragon patron" : {
-					name : "Brass Dragon Patron",
-					description : "\n   " + "I have draconic ancestry with brass dragons, which are affiliated with fire damage" + "\n   " + "When interacting with dragons, if I can add my proficiency bonus, I can double it",
-					eval : 	"var ToAdd = [\"warlock\", \"subclassfeature6\", \"brass\"]; " + 
-							"if (classes.known.warlock.level >= 6 && tDoc.getField(\"Class Features Remember\").value.indexOf(ToAdd.toString()) === -1) { " + 
-								"ClassFeatureOptions(ToAdd); " +
-							"} ",
-					dragonElement : "fire"
-				},
-				"bronze dragon patron" : {
-					name : "Bronze Dragon Patron",
-					description : "\n   " + "I have draconic ancestry with bronze dragons, which are affiliated with lightning dmg" + "\n   " + "When interacting with dragons, if I can add my proficiency bonus, I can double it",
-					eval : 	"var ToAdd = [\"warlock\", \"subclassfeature6\", \"bronze\"]; " + 
-							"if (classes.known.warlock.level >= 6 && tDoc.getField(\"Class Features Remember\").value.indexOf(ToAdd.toString()) === -1) { " + 
-								"ClassFeatureOptions(ToAdd); " + 
-							"} ",
-					dragonElement : "lightning"
-				},
-				"copper dragon patron" : {
-					name : "Copper Dragon Patron",
-					description : "\n   " + "I have draconic ancestry with copper dragons, which are affiliated with acid damage" + "\n   " + "When interacting with dragons, if I can add my proficiency bonus, I can double it",
-					eval : 	"var ToAdd = [\"warlock\", \"subclassfeature6\", \"copper\"]; " + 
-							"if (classes.known.warlock.level >= 6 && tDoc.getField(\"Class Features Remember\").value.indexOf(ToAdd.toString()) === -1) { " + 
-								"ClassFeatureOptions(ToAdd); " + 
-							"} ",
-					dragonElement : "acid"
-				},
-				"gold dragon patron" : {
-					name : "Gold Dragon Patron",
-					description : "\n   " + "I have draconic ancestry with gold dragons, which are affiliated with fire damage" + "\n   " + "When interacting with dragons, if I can add my proficiency bonus, I can double it",
-					eval : 	"var ToAdd = [\"warlock\", \"subclassfeature6\", \"gold\"]; " + 
-							"if (classes.known.warlock.level >= 6 && tDoc.getField(\"Class Features Remember\").value.indexOf(ToAdd.toString()) === -1) { " + 
-								"ClassFeatureOptions(ToAdd); " + 
-							"} ",
-					dragonElement : "fire"
-				},
-				"green dragon patron" : {
-					name : "Green Dragon Patron",
-					description : "\n   " + "I have draconic ancestry with green dragons, which are affiliated with poison damage" + "\n   " + "When interacting with dragons, if I can add my proficiency bonus, I can double it",
-					eval : 	"var ToAdd = [\"warlock\", \"subclassfeature6\", \"green\"]; " + 
-							"if (classes.known.warlock.level >= 6 && tDoc.getField(\"Class Features Remember\").value.indexOf(ToAdd.toString()) === -1) {" + 
-								"ClassFeatureOptions(ToAdd); " +
-							"} ",
-					dragonElement : "poison"
-				},
-				"red dragon patron" : {
-					name : "Red Dragon Patron",
-					description : "\n   " + "I have draconic ancestry with red dragons, which are affiliated with fire damage" + "\n   " + "When interacting with dragons, if I can add my proficiency bonus, I can double it",
-					eval :	"var ToAdd = [\"warlock\", \"subclassfeature6\", \"red\"]; " + 
-							"if (classes.known.warlock.level >= 6 && tDoc.getField(\"Class Features Remember\").value.indexOf(ToAdd.toString()) === -1) { " + 
-								"ClassFeatureOptions(ToAdd); " +
-							"} ",
-					dragonElement : "fire"
-				},
-				"silver dragon patron" : {
-					name : "Silver Dragon Patron",
-					description : "\n   " + "I have draconic ancestry with silver dragons, which are affiliated with cold damage" + "\n   " + "When interacting with dragons, if I can add my proficiency bonus, I can double it",
-					eval : 	"var ToAdd = [\"warlock\", \"subclassfeature6\", \"silver\"]; " + 
-							"if (classes.known.warlock.level >= 6 && tDoc.getField(\"Class Features Remember\").value.indexOf(ToAdd.toString()) === -1) {" + 
-								"ClassFeatureOptions(ToAdd); " + 
-							"} ",
-					dragonElement : "cold"
-				},
-				"white dragon patron" : {
-					name : "White Dragon Patron",
-					description : "\n   " + "I have draconic ancestry with white dragons, which are affiliated with cold damage" + "\n   " + "When interacting with dragons, if I can add my proficiency bonus, I can double it",
-					eval : 	"var ToAdd = [\"warlock\", \"subclassfeature6\", \"white\"]; " + 
-							"if (classes.known.warlock.level >= 6 && tDoc.getField(\"Class Features Remember\").value.indexOf(ToAdd.toString()) === -1) { " + 
-								"ClassFeatureOptions(ToAdd); " + 
-							"} ",
-					dragonElement : "cold"
-				}
+			name : "Dragon Patron",
+			source : ["HB", 0],
+			minlevel : 1,
+			description : "\n   " + "Choose an Ancient Dragon Patron using the \"Choose Feature\" button above.",
+			choices : ["Black Dragon Patron", "Blue Dragon Patron", "Brass Dragon Patron", "Bronze Dragon Patron", "Copper Dragon Patron", "Gold Dragon Patron", "Green Dragon Patron", "Red Dragon Patron", "Silver Dragon Patron", "White Dragon Patron"],
+			"black dragon patron" : {
+			name : "Black Dragon Patron",
+				description : "\n   " + "My patron is an ancient black dragon, which is affiliated with acid damage.",
+				eval : 	"var ToAdd = [\"warlock\", \"subclassfeature6\", \"black\"]; " + 
+						"if (classes.known.warlock.level >= 6 && tDoc.getField(\"Class Features Remember\").value.indexOf(ToAdd.toString()) === -1) { " + 
+							"ClassFeatureOptions(ToAdd); " + 
+						"} ",
+				dragonElement : "acid"
+			},
+			"blue dragon patron" : {
+				name : "Blue Dragon Patron",
+				description : "\n   " + "My patron is an ancient blue dragon, which is affiliated with lightning damage.",
+				eval : 	"var ToAdd = [\"warlock\", \"subclassfeature6\", \"blue\"]; " + 
+						"if (classes.known.warlock.level >= 6 && tDoc.getField(\"Class Features Remember\").value.indexOf(ToAdd.slice(0,3).toString()) === -1) { " + 
+							"ClassFeatureOptions(ToAdd); " +
+						"} ",
+				dragonElement : "lightning"
+			},
+			"brass dragon patron" : {
+				name : "Brass Dragon Patron",
+				description : "\n   " + "My patron is an ancient brass dragon, which is affiliated with fire damage.",
+				eval : 	"var ToAdd = [\"warlock\", \"subclassfeature6\", \"brass\"]; " + 
+						"if (classes.known.warlock.level >= 6 && tDoc.getField(\"Class Features Remember\").value.indexOf(ToAdd.toString()) === -1) { " + 
+							"ClassFeatureOptions(ToAdd); " +
+						"} ",
+				dragonElement : "fire"
+			},
+			"bronze dragon patron" : {
+				name : "Bronze Dragon Patron",
+				description : "\n   " + "My patron is an ancient bronze dragon, which is affiliated with lightning dmg.",
+				eval : 	"var ToAdd = [\"warlock\", \"subclassfeature6\", \"bronze\"]; " + 
+						"if (classes.known.warlock.level >= 6 && tDoc.getField(\"Class Features Remember\").value.indexOf(ToAdd.toString()) === -1) { " + 
+							"ClassFeatureOptions(ToAdd); " + 
+						"} ",
+				dragonElement : "lightning"
+			},
+			"copper dragon patron" : {
+				name : "Copper Dragon Patron",
+				description : "\n   " + "My patron is an ancient copper dragon, which is affiliated with acid damage.",
+				eval : 	"var ToAdd = [\"warlock\", \"subclassfeature6\", \"copper\"]; " + 
+						"if (classes.known.warlock.level >= 6 && tDoc.getField(\"Class Features Remember\").value.indexOf(ToAdd.toString()) === -1) { " + 
+							"ClassFeatureOptions(ToAdd); " + 
+						"} ",
+				dragonElement : "acid"
+			},
+			"gold dragon patron" : {
+				name : "Gold Dragon Patron",
+				description : "\n   " + "My patron is an ancient gold dragon, which is affiliated with fire damage.",
+				eval : 	"var ToAdd = [\"warlock\", \"subclassfeature6\", \"gold\"]; " + 
+						"if (classes.known.warlock.level >= 6 && tDoc.getField(\"Class Features Remember\").value.indexOf(ToAdd.toString()) === -1) { " + 
+							"ClassFeatureOptions(ToAdd); " + 
+						"} ",
+				dragonElement : "fire"
+			},
+			"green dragon patron" : {
+				name : "Green Dragon Patron",
+				description : "\n   " + "My patron is an ancient green dragon, which is affiliated with poison damage.",
+				eval : 	"var ToAdd = [\"warlock\", \"subclassfeature6\", \"green\"]; " + 
+						"if (classes.known.warlock.level >= 6 && tDoc.getField(\"Class Features Remember\").value.indexOf(ToAdd.toString()) === -1) {" + 
+							"ClassFeatureOptions(ToAdd); " +
+						"} ",
+				dragonElement : "poison"
+			},
+			"red dragon patron" : {
+				name : "Red Dragon Patron",
+				description : "\n   " + "My patron is an ancient red dragon, which is affiliated with fire damage.",
+				eval :	"var ToAdd = [\"warlock\", \"subclassfeature6\", \"red\"]; " + 
+						"if (classes.known.warlock.level >= 6 && tDoc.getField(\"Class Features Remember\").value.indexOf(ToAdd.toString()) === -1) { " + 
+							"ClassFeatureOptions(ToAdd); " +
+						"} ",
+				dragonElement : "fire"
+			},
+			"silver dragon patron" : {
+				name : "Silver Dragon Patron",
+				description : "\n   " + "My patron is an ancient silver dragon, which is affiliated with cold damage.",
+				eval : 	"var ToAdd = [\"warlock\", \"subclassfeature6\", \"silver\"]; " + 
+						"if (classes.known.warlock.level >= 6 && tDoc.getField(\"Class Features Remember\").value.indexOf(ToAdd.toString()) === -1) {" + 
+							"ClassFeatureOptions(ToAdd); " + 
+						"} ",
+				dragonElement : "cold"
+			},
+			"white dragon patron" : {
+				name : "White Dragon Patron",
+				description : "\n   " + "My patron is an ancient white dragon, which is affiliated with cold damage.",
+				eval : 	"var ToAdd = [\"warlock\", \"subclassfeature6\", \"white\"]; " + 
+						"if (classes.known.warlock.level >= 6 && tDoc.getField(\"Class Features Remember\").value.indexOf(ToAdd.toString()) === -1) { " + 
+							"ClassFeatureOptions(ToAdd); " + 
+						"} ",
+				dragonElement : "cold"
+			}
 		},
 		"subclassfeature6" : {
 			name : "Ancient's Breath",
@@ -140,10 +140,11 @@ ClassSubList["ancient dragon"] = {
 			description : "\n   " + 
 				"I gain a breath weapon based on my draconic patron's element and area of effect." + "\n   " + 
 				"I can expend a spell slot of at least 3rd level and use this breath weapon." + "\n   " +
-				"All creatures in the area of my breath wweapon make a Dexterity save against my spell save DC" + "\n   " + 
+				"All creatures in the area of my breath weapon make a Dexterity save against my spell save DC" + "\n   " + 
 				"or a Constitution save if the breath deals Cold or Poison damage." + "\n   " + 
 				"On a successful save, creatures in the area take half damage." + "\n   " + 
 				"Damage is equal to 4d6 + my Charisma modifier, plus 1d8 damage per spell level expended above 3rd.",
+			
 			calcChanges : {
 					atkAdd : [
 						"if (WeaponName === 'breath weapon' && CurrentRace.known === 'dragonborn' && CurrentRace.variant) { " + 
