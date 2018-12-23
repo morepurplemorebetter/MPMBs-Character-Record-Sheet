ea6fd74ca81415bbaf9409bf7a7347b0cfcbbd1a
morepurplemorebetter <flapkan@gmail.com>
2018-12-23 18:51:00 +0100

    v13.0.0beta7 - Update MagicItemList with actual magic items
    There are currently only 4 magic items that serve as examples

--- _variables/ListsMagicItems.js
+++ _variables/ListsMagicItems.js
@@ -2,46 +2,72 @@ var Base_MagicItemsList = {
 	"alchemy jug" : {
 		name : "Alchemy Jug",
 		source : ["D", 150],
-		description : "As an action, command the jug to produce liquid; or an action to uncorked it and pour 2 gal/min. After producing, it only makes the same up to its max, until next dawn. Oil (1 qt), acid (8 fl oz), basic poison (1/2 fl oz), beer (4 gal), honey/wine (1 gal), fresh water (8 gal), mayonnaise/vinegar (2 gal), salt water (12 gal).",
-		descriptionLong : "A heavy ceramic jug. As an action, the jug can be commanded to hold a chosen liquid. With another action, I can uncork the jug and pour the liquid out at 2 gallons per minute. Once commanded to produce a liquid, it can't produce a different one or more than the maximum of one, until the next dawn.\rLiquids (with maximum): acid (8 fl. oz.), basic poison (1/2 fl. oz.), beer (4 gallons), honey (1 gallon), mayonnaise (2 gallons), oil (1 quart), vinegar (2 gallons), fresh water (8 gallons), salt water (12 gallons), wine (1 gallon).",
 		type : "wondrous item",
 		rarity : "uncommon",
 		attunement : false,
-		allowDuplicates : false,
+		description : "As an action, command the jug to produce liquid; or an action to uncorked it and pour 2 gal/min. After producing, it only makes the same up to its max, until next dawn. Oil (1 qt), acid (8 fl oz), basic poison (1/2 fl oz), beer (4 gal), honey/wine (1 gal), fresh water (8 gal), mayonnaise/vinegar (2 gal), salt water (12 gal).",
 		weight : 12,
+		descriptionLong : "A heavy ceramic jug. As an action, the jug can be commanded to hold a chosen liquid. With another action, I can uncork the jug and pour the liquid out at 2 gallons per minute. Once commanded to produce a liquid, it can't produce a different one or more than the maximum of one, until the next dawn.\rLiquids (with maximum): acid (8 fl. oz.), basic poison (1/2 fl. oz.), beer (4 gallons), honey (1 gallon), mayonnaise (2 gallons), oil (1 quart), vinegar (2 gallons), fresh water (8 gallons), salt water (12 gallons), wine (1 gallon).",
+		allowDuplicates : false,
 		descriptionFull : "This ceramic jug appears to be able to hold a gallon of liquid and weighs 12 pounds whether full or empty. Sloshing sounds can be heard from within the jug when it is shaken, even if the jug is empty." + "\n   " + "You can use an action and name one liquid from the table below to cause the jug to produce the chosen liquid. Afterward, you can uncork the jug as an action and pour that liquid out, up to 2 gallons per minute. The maximum amount of liquid the jug can produce depends on the liquid you named." + "\n   " + "Once the jug starts producing a liquid, it can't produce a different one, or more of one that has reached its maximum, until the next dawn.\n\n" + toUni("Max") + "\t" + toUni("Liquid") + "\t\t" + toUni("Max") + "\t" + toUni("Liquid") + "\n8 ounces\tAcid\t\t1 quart\tOil\n1/2 ounce\tBasic poison\t2 gallons\tVinegar\n4 gallons\tBeer\t\t8 gallons\tWater, fresh\n1 gallon\tHoney\t\t12 gallons\tWater, salt\n2 gallons\tMayonnaise\t1 gallon\tWine"
 	},
-	// DEBUGGING/TESTING:
-	"test - gives language" : {
-		name : "Language Giver",
-		type : "wondrous item",
+	// example magical armour
+	"glamoured studded leather" : {
+		name : "Glamoured Studded Leather",
+		source : [["SRD", 224], ["D", 172]],
+		type : "armor (studded leather)",
 		rarity : "rare",
-		source : ["SRD", 12],
-		description : "This magic item grants the proficiency with the 'MPMB language' language.",
-		weight : 12,
+		attunement : false,
+		description : "Studded leather with +1 AC. As a bonus action, I can speak its command word and have it assume the appearance of a normal set of clothing or another armor. I decide what it looks like: style, color, and accessories, but the armor retains its bulk and weight. The illusion lasts until I use this again or remove the armor.",
+		weight : 13,
+		descriptionFull : "While wearing this armor, you gain a +1 bonus to AC. You can also use a bonus action to speak the armor's command word and cause the armor to assume the appearance of a normal set of clothing or some other kind of armor. You decide what it looks like, including color, style, and accessories, but the armor retains its normal bulk and weight. The illusory appearance last until you use this property again or remove the armor.",
+		addArmor : "Glamoured Studded Leather",
+		armorOptions : {
+			regExpSearch : /^(?=.*glamou?r)(?=.*(studded|studs))(?=.*leather).*$/i,
+			name : "Glamoured studded Leather",
+			source : [["SRD", 224], ["D", 172]],
+			type : "light",
+			ac : 13, // 1 more AC than normal studded leather because of the +1 magical bonus
+			weight : 13
+		},
+		action : [["bonus action", ""]]
+	},
+	// example magical weapon
+	"dwarven thrower" : {
+		name : "Dwarven Thrower",
+		source : [["SRD", 220], ["D", 167]],
+		type : "weapon (warhammer)",
+		rarity : "very rare",
 		attunement : true,
-		languageProfs : ['MPMB language'],
-		descriptionFull : "Superleuk met een taal!"
-		
+		description : "This magical warhammer adds a +3 bonus to attack and damage rolls made with it. It has the thrown property with a normal range of 20 ft and a long range of 60 ft. It deals an extra 1d8 damage (or 2d8 if the target is a giant) when thrown. Immediately after the attack, the weapon flies back to my hand.",
+		prerequisite : "Requires attunement by a dwarf",
+		prereqeval : "CurrentRace.known.indexOf('dwarf') !== -1",
+		weight : 2,
+		descriptionFull : "You gain a +3 bonus to attack and damage rolls made with this magic weapon. It has the thrown property with a normal range of 20 feet and a long range of 60 feet. When you hit with a ranged attack using this weapon, it deals an extra 1d8 damage or, if the target is a giant, 2d8 damage. Immediately after the attack, the weapon flies back to your hand.",
+		addWeapons : ["Dwarven Thrower"],
+		weaponOptions : {
+			regExpSearch : /^(?=.*dwarven)(?=.*thrower).*$/i,
+			name : "Dwarven Thrower",
+			source : [["SRD", 220], ["D", 167]],
+			list : "melee",
+			ability : 1,
+			type : "Martial",
+			damage : [1, 8, "bludgeoning"],
+			range : "Melee, 20/60 ft",
+			weight : 2,
+			description : "Thrown, versatile (1d10); +1d8 damage when thrown (2d8 vs. giant) and returns immediately",
+			abilitytodamage : true,
+			modifiers : [3, 3] // add 3 to each to hit and damage because of the magical bonus
+		}
 	},
-	"test - gives tool" : {
-		name : "Tool Giver",
-		type : "wondrous item",
-		rarity : "common",
-		source : ["SRD", 901],
-		description : "This magic item grants proficiency with the 'MPMB tool' tool.",
-		toolProfs : ['MPMB tool'],
-		descriptionFull : "Superleuk met een gereedschap!"
-		
-	},
-	"test - gives martial weapon proficiency" : {
-		name : "Martial Giver",
-		type : "wondrous item",
-		rarity : "legendary",
-		source : ["SRD", 1],
-		calculate : 'event.value = "This magic item grants proficiency with martial weapons."',
-		weaponProfs : [false, true],
-		descriptionFull : "Superleuk met een wapen!"
-		
+	"ring of swimming" : {
+		name : "Ring of Swimming",
+		source : [["SRD", 238], ["D", 193]],
+		type : "ring",
+		rarity : "uncommon",
+		attunement : false,
+		description : "I have a swimming speed of 40 feet while wearing this ring.",
+		descriptionFull : "You have a swimming speed of 40 feet while wearing this ring.",
+		speed : { swim : { spd : 40, enc : 30 } }
 	}
 };
